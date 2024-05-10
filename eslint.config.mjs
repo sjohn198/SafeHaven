8d41764248d004a588f5d40f27afeecc99c258bc
Cameron <cameron.maloney@warriorlife.net>
2024-05-10 11:42:45 -0700

    Eslint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+const customRules = {
+  "no-undef": "warn",
+  // feel free to add more rules
+};
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  { ...pluginJs.configs.recommended, rules: { ...pluginJs.configs.recommended.rules, ...customRules }, settings: { react: { version: "detect"}}},
+  pluginReactConfig,
+];
